68c4263636576c7fb44d868197e0ef749ecb384a
阿黒直也 <aguronaoya@naoya-aguronoMacBook-Air.local>
2019-11-10 18:04:23 +0900

    write MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+	get "/" => "root#top"
+	get "/books" => "books#index",as:"books"
+	post "/books" => "books#create"
+	get "/books/:id/edit" => "books#edit",as:"edit_book"
+	get "/books/:id" => "books#show",as:"book"
+	patch "/books/:id" => "books#update"
+	delete "/books/:id" => "books#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
